11d471a5425e8d162f1fdc03594b075ba716c06a
hotl1ne <m.kurychja@gmail.com>
2024-04-17 13:55:03 +0300

    changes add

--- src/app/components/chat-page/chat-page.component.ts
+++ src/app/components/chat-page/chat-page.component.ts
@@ -11,6 +11,8 @@ export class ChatPageComponent implements OnInit{
 
   messages: { text: string, type: string }[] = [];
   
+  selected_ChatIndex: number = 0;
+
   selectedChat: ChatModel = {
     userId: '',
     chats: [],
@@ -32,6 +34,15 @@ export class ChatPageComponent implements OnInit{
     const messageInput = document.getElementById('message_input') as HTMLInputElement;
     const message = messageInput.value;
     this.messages.unshift({ text: message, type: 'user_message' });
+
+    var id = localStorage.getItem("chatId");
+    if (id) {
+      var string = JSON.stringify(this.messages)
+
+        localStorage.setItem(id, string);
+    }
+      console.log(this.messages);
+
     messageInput.value = '';
   }
 
@@ -46,6 +57,7 @@ export class ChatPageComponent implements OnInit{
 
   select_Chat(i: any)
   {
+    localStorage.setItem("chatId", i);
     this.selectedChat = {
       userId: this.chat.userId,
       chats: this.chat.chats.length > 0 ? [this.chat.chats[i]] : [],
@@ -53,6 +65,20 @@ export class ChatPageComponent implements OnInit{
       fromUserMessage: this.chat.fromUserMessage.length > 0 ? [this.chat.fromUserMessage[i]] : []
     };
     this.screenWidth = this.elRef.nativeElement.ownerDocument.defaultView.innerWidth;
+    
+    var id = localStorage.getItem("chatId");
+    if (id) {
+      const storedMessages = localStorage.getItem(id);
+      if (storedMessages) {
+        this.messages = JSON.parse(storedMessages);
+      } else {
+        this.messages = []; 
+      }
+    } else {
+      this.messages = [];
+    }
+      console.log(this.messages);
+
     if(this.screenWidth <= 768)
       {
         var recentChats = document.getElementById('recent_chats');
--- src/app/parts/side-menu/side-menu.component.ts
+++ src/app/parts/side-menu/side-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
+
+  constructor(private route: Router) {}
+
+  Logout() {
+    localStorage.clear();
+    this.route.navigate([""]);
+  }
 
 }
